569a1ddf60681e401f49c8a0acbcae6f75c28458
pipclubbs <70511582+pipclubbs@users.noreply.github.com>
2021-08-08 08:56:57 +0100

    added footer and dictionary component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from "./pip.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header d-flex">
-        <img src={logo} alt="logo" className="img-fluid" />
-        <span className="App-header-text">Hello, world!</span>
-      </header>
+      <div className="container">
+        <header className="App-header d-flex">
+          <img src={logo} alt="logo" className="img-fluid" />
+          <span className="App-header-text">Dictionary</span>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <span>
+            <a
+              className="App-footer-img"
+              href="https://github.com/pipclubbs/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-source code
+            </a>{" "}
+            {""} by Phillippa Clubbs
+          </span>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  return (
+    <div className="Dictionary">
+      <span className="Dictionary-text">
+        the dictionary search form will be here
+      </span>
+    </div>
+  );
+}
