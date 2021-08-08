761387686be8b5bed3ac96a79c6866cc1a0476ba
pipclubbs <70511582+pipclubbs@users.noreply.github.com>
2021-08-08 08:35:52 +0100

    updating header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
+import logo from "./pip.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello, world</header>
+      <header className="App-header d-flex">
+        <img src={logo} alt="logo" className="img-fluid" />
+        <span className="App-header-text">Hello, world!</span>
+      </header>
     </div>
   );
 }
