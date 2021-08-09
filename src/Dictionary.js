fd38d5c0aebd72e4112d2530af5cecbbe78cace0
pipclubbs <70511582+pipclubbs@users.noreply.github.com>
2021-08-09 21:57:05 +0100

    added photo API and Photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,33 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response.data.photos);
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000011ec856938a234d878ac1fe0f62f08b91";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -33,6 +46,7 @@ export default function Dictionary() {
       </form>
 
       <Results data={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
---
+++ src/Photos.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <div className="Photos">
+        <section>
+          <div className="row">
+            {props.photos.map(function (photo, index) {
+              return (
+                <div className="col-4">
+                  <a href={photo.src.original} target="_blank" rel="noreferrer">
+                    <img
+                      src={photo.src.landscape}
+                      alt="whatever the keyword was"
+                      key={index}
+                    />
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
